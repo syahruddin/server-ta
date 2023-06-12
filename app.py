640c349506bc3d5827a64fa1175d7e82338493f4
syahruddin <syahruddinr@gmail.com>
2023-06-12 13:00:12 +0700

    testing database

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
 from flask import Flask
+import json
+import mysql.connector  as mysql
+
+mydb = mysql.connect(
+  host="127.0.0.1",
+  port="3306",
+  user="root",
+  password="",
+  database="Log"
+)
+
+cursor = mydb.cursor()
+
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return "hello world"
+
+@app.route('/test')
+def test():
+    cursor.execute("select * from access where status_code = 400;")
+    #print(cursor.fetchall)
+    temp = ""
+    
+    for row in cursor:
+        x = 
+    return temp
+
+
 if __name__ == "__main__":
     app.run()
           
